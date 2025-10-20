80da7ffa17c1e51948227f75848ef999deb864f4
thallys-yaslip <thallys.yaslip@gmail.com>
2025-10-20 16:30:37 -0300

    update: alterações

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home - Index</title>
     <link rel="stylesheet" href="./scss/style.css">
+    <link rel="stylesheet" href="./CompiledCss/style.css">
+    <link rel="stylesheet" href="./CompiledCss/components.css">
     <link rel="stylesheet" href="./scss/components.css">
 </head>
 
@@ -13,7 +15,31 @@
     <div class="container_principal">
         <div class="scopo">
             <div class="left_side">
-                <img src="./assets/logo.png" alt="">
+                <img class="img_left_side" src="./assets/logo.png" alt="">
+                <div class="container_principal_left_side">
+
+                    <ul>
+                        <li class="icon_img active">
+                            <img src="./assets/icons/sidebar/home.png" alt="">
+                        </li>
+                        <li class="icon_img">
+                            <img src="./assets/icons/sidebar/stadium.png" alt="">
+                        </li>
+                        <li class="icon_img">
+                            <img src="./assets/icons/sidebar/shield.png" alt="">
+                        </li>
+                        <li class="icon_img">
+                            <img src="./assets/icons/sidebar/blockchain.png" alt="">
+                        </li>
+                    </ul>
+
+                </div>
+
+                <div class="container_logout">
+                    <a href="">
+                        <img src="./assets/icons/sidebar/exit.png" alt="">
+                    </a>
+                </div>
             </div>
             <div class="middle_side">
                 <div class="topo_middle">
@@ -34,10 +60,47 @@
                 </div>
             </div>
             <div class="right_side">
-                <div class="avatar">
-                    <img src="./assets/logo.png" alt="">
+                <div class="container_members">
+                    <div class="avatar">
+                        <img src="./assets/logo.png" alt="">
+                    </div>
 
+                    <ul class="members_equipe">
+                        <li class="member">
+                            <img src="./assets/logo.png" alt="">
+
+                        </li>
+                        <li class="member">
+                            <img src="./assets/logo.png" alt="">
+
+                        </li>
+                        <li class="member">
+                            <img src="./assets/logo.png" alt="">
+
+                        </li>
+                        <li class="member">
+                            <img src="./assets/logo.png" alt="">
+
+                        </li>
+                        <li class="member">
+                            <img src="./assets/logo.png" alt="">
+
+                        </li>
+                    </ul>
                 </div>
+
+                <ul class="social">
+                    <li>
+                        <a href=""><img src="./assets/icons/discord.png" alt=""></a>
+                    </li>
+                    <li>
+                        <a href=""><img src="./assets/icons/whatsapp.png" alt=""></a>
+                    </li>
+                    <li>
+                        <a href=""><img src="./assets/icons/instagram.png" alt=""></a>
+                    </li>
+                </ul>
+
             </div>
         </div>
     </div>
